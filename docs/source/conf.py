bdbcacb86d49328a6fc23597134f36b1b6ee12e6
Alexankitty <alexankitty@gmail.com>
2023-06-16 20:40:08 -0600

    fix name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    "sphinx.ext.opengraph",
+    "sphinxext.opengraph",
 ]
 
 # -- OGP Configuration
